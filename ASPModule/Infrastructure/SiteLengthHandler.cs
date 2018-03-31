850958bb3524dbdd959e5b76280c2111cc048f91
dawid Stoga <daw2@o2.pl>
2018-03-31 19:45:33 +0200

    Asynchronous Handlers

---
+++ ASPModule/Infrastructure/SiteLengthHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ASPModule.Infrastructure
+{
+    public class SiteLengthHandler :HttpTaskAsyncHandler
+    {
+        public override async Task ProcessRequestAsync(HttpContext context)
+        {
+            string data = await new HttpClient().GetStringAsync("http://www.apress.com");
+            context.Response.ContentType = "text/html";
+            context.Response.Write(string.Format("<span>Lenght: {0}</span>", data.Length));
+
+        }
+
+    }
+    
+}
