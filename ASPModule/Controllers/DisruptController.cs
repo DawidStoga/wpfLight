9db47fc97803a0ab255210b6b4eb43296cc8272c
dawid Stoga <daw2@o2.pl>
2018-03-31 23:20:34 +0200

    Disrupting the Request Life Cycle - Simplifiying the redirection process

---
+++ ASPModule/Controllers/DisruptController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPModule.Controllers
+{
+    public class DisruptController : Controller
+    {
+        // GET: Disrupt
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    public ActionResult  LogIn()
+    {
+        return RedirectToAction("Index", "Disrupt");
+    }
+    }
+
+
+
+}
---
+++ ASPModule/Infrastructure/disruptingLifeCycle/RedirectModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASPModule.Infrastructure.disruptingLifeCycle
+{
+    public class RedirectModule : IHttpModule
+    {
+        public void Dispose()
+        {
+          
+        }
+
+        public void Init(HttpApplication context)
+        {
+
+            context.MapRequestHandler += (o, e) =>
+            {
+                if (context.Request.Url.AbsoluteUri.Contains("LogIn"))
+                {
+                   // context.Response.Redirect("/Disrupt/Index");  OR
+                    RouteValueDictionary rvd = context.Request.RequestContext.RouteData.Values;
+                    //rvd.Where(x => x.Key == "Action" && x.Value == "LogIn").FirstOrDefault();
+                    if (rvd.Any(x => x.Key == "action" && (string)x.Value == "LogIn"))
+                    {
+                        string url = UrlHelper.GenerateUrl("", "Index", "Disrupt", rvd, RouteTable.Routes,
+                            context.Context.Request.RequestContext, false);
+                      //  context.Response.Redirect("/Disrupt/Index");
+                        context.Response.Redirect(url);
+                    }
+                }
+            };
+        }
+    }
+}
