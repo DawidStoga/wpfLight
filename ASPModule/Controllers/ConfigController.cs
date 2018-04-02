ee7beaca898fe7e32974f025f84971e64c1d87ab
dawid Stoga <daw2@o2.pl>
2018-04-02 18:01:30 +0200

    Set Configuration - new  usee default section

---
+++ ASPModule/Controllers/ConfigController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using ASPModule.Models;
+
+namespace ASPModule.Controllers
+{
+    public class ConfigController : Controller
+    {
+        // GET: Config
+        public ActionResult Index()
+        {
+            Dictionary<string,string> configData = new Dictionary<string, string>();
+           
+            foreach (string key in WebConfigurationManager.AppSettings)
+            {
+                configData.Add(key, WebConfigurationManager.AppSettings[key]);
+            }
+           
+         
+
+            return View(configData);
+        }
+
+        public ActionResult ShowConfigs()
+        {
+            ConfigModelView config = new ConfigModelView();
+            return View(config);
+        }
+    }
+}
---
+++ ASPModule/Models/ConfigModelView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace ASPModule.Models
+{
+    public class ConfigModelView
+    {
+        public NameValueCollection AppSettings { get; set; }
+        public ConnectionStringSettingsCollection ConnectionStrings { get; set; }
+
+    }
+}
