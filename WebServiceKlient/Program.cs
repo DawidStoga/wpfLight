b7766b75b3a58e41c3abd25ba01de10cfbc77da6
dawid Stoga <daw2@o2.pl>
2018-04-10 22:47:55 +0200

    WSDL - klient - local serwer

---
+++ WebServiceKlient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServiceKlient.localhost;
+
+namespace WebServiceKlient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            int b = 5;
+            var ws = new MathService();
+            ws.UserAuthValue = new UserAuth() { UserName = "Odyn", Password = "itcraftsman" };
+            var wynik = ws.Multiply(a, b);
+            Console.WriteLine(wynik);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebServices/MathService.asmx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace WebServices
+{
+    /// <summary>
+    /// Summary description for MathService
+    /// </summary>
+    [WebService(Namespace = "http://itcraftsman.pl")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MathService : System.Web.Services.WebService
+    {
+        public UserAuth MyUserAuth { get; set; }
+
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod (Description =("Mnozeie liczb"))]
+        [SoapHeader("MyUserAuth")]
+        public double Multiply(double a, int b)
+        {
+            if (AuthorizeUser(MyUserAuth) == true)
+                return a * b;
+            else return 9999;
+        }
+
+        private static bool AuthorizeUser(UserAuth usr)
+        {
+            if (usr.UserName == "Odyn" && usr.Password == "itcraftsman")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class UserAuth : SoapHeader
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+}
